a187f24c73ac1b8561064ba8b70d01f9d08dce23
Andrew Halisky <ajhalisky@gmail.com>
2026-01-18 23:10:06 -0500

    Finished week 02 .NET project

--- Documents/BYU Idaho 2025-2026/CSE 325 - .NET Software Development - Winter 2026/Week_02/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ Documents/BYU Idaho 2025-2026/CSE 325 - .NET Software Development - Winter 2026/Week_02/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+638417575990b21e78567ee7edb036994c306555")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682b81e9271149044179b40f1a05ca101e0948f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
